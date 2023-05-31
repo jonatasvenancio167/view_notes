e8eedc800f6064256cda61c1c38b922b77bb203e
Samila Barros <jvenancio167@hotmail.com>
2023-05-31 20:28:36 -0300

    change studant model

--- models/studant.py
+++ models/studant.py
@@ -3,9 +3,12 @@ from db import db
 class StudantModel(db.Model):
 	__tablename__ = 'studants'
 	__table_args__ = {'sqlite_autoincrement': True}
-	id = db.Column(db.Integer, primary_key=True)
+ 
+	id = db.Column(db.Integer, unique=True, nullable=False)
+	cpf = db.Column(db.String, unique=True, primary_key=True)
 	name = db.Column(db.String(255), nullable=False)
 	birthdate = db.Column(db.String, nullable=False)
-	status = db.Column(db.Boolean, nullable=False)
-	enrollment = db.Column(db.String(13), unique=True, nullable=False)
-	
+	sex = db.Column(db.String, nullable=False)
+	age = db.Column(db.String, nullable=False)
+ 
+	notes = db.relationship("NoteModel", back_populates="studant")
