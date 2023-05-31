052f8036538e432e4024a8df027c244eece2f66d
Samila Barros <jvenancio167@hotmail.com>
2023-05-31 20:27:21 -0300

    create relationship and return with request data

--- models/note.py
+++ models/note.py
@@ -1,10 +1,31 @@
 from db import db
+from sqlalchemy import text
 
 class NoteModel(db.Model):
-	__tablename__ = 'studants'
-	__table_args__ = {'sqlite_autoincrement': True}
-	id = db.Column(db.Integer, primary_key=True)
-	nota_1 = db.Column(db.Float, nullable=True)
-	nota_2 = db.Column(db.Float, nullable=True)
-	nota_final = db.Column(db.Float, nullable=True)
-	
+  __tablename__ = 'notes'
+  __table_args__ = {'sqlite_autoincrement': True}
+
+  id = db.Column(db.Integer, unique=True, primary_key=True)
+  note_1 = db.Column(db.Float(precision=2), default=0.0)
+  note_2 = db.Column(db.Float(precision=2), default=0.0)
+  finally_note = db.Column(db.Float(precision=2), default=0.0)
+  
+  studant_id = db.Column(
+		db.Integer, db.ForeignKey("studants.id"), nullable=False
+  )
+  studant = db.relationship("StudantModel", back_populates="notes")
+  
+  def serialize_with_student(self):
+    return {
+      "id": self.id,
+      "note_1": self.note_1,
+      "note_2": self.note_2,
+      "finally_note": self.finally_note,
+      "student": {
+          "cpf": self.studant.cpf,
+          "name": self.studant.name,
+          "birthdate": self.studant.birthdate,
+          "sex": self.studant.sex,
+          "age": self.studant.age
+        } if self.studant else None
+  }
