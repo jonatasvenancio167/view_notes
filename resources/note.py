62c46537d0bf32b37da9207d3cbd39e9e51b3dc9
Samila Barros <jvenancio167@hotmail.com>
2023-05-31 20:31:03 -0300

    change note resource

--- resources/note.py
+++ resources/note.py
@@ -2,49 +2,63 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
+from flask import jsonify
+
 from db import db
 from models import NoteModel
-from schemas import PlainStudant, StudantUpdateSchema
+from schemas import PlainNoteSchema, NoteSchema
 
 blp = Blueprint("Notes", "notes", description="Operations on notes")
 
 @blp.route("/note/<string:note_id>")
 class Note(MethodView):
-  @blp.response(200, PlainStudant)
+  @blp.response(200, PlainNoteSchema)
   def get(self, note_id):
     note = NoteModel.query.get_or_404(note_id)
-    return note
+    return jsonify(note.serialize_with_student())
   
-  @blp.arguments(StudantUpdateSchema)
-  @blp.response(200, PlainStudant)
+  def delete(self, note_id):
+    note = NoteModel.query.filter_by(id=note_id).first_or_404()
+    db.session.delete(note)
+    db.session.commit()
+    return { "message": "Note deleted." }
+  
+  @blp.response(200, PlainNoteSchema)
   def put(self, note_data, note_id):
-    note = NoteModel.query.get(note_id)
+    avaliation = NoteModel.query.get(note_id)
     
-    if note:
-      note.nota_1 = note_data["nota_1"]
-      note.nota_2 = note_data["nota_2"]
-      note.nota_final = note_data["nota_final"]
+    if avaliation:
+      avaliation.note_1 = note_data["note_1"]
+      avaliation.note_2 = note_data["note_2"]
+      avaliation.finally_note = note_data["finally_note"]
     else:
-      note = NoteModel(id=note_id, **note_data)
+      avaliation = NoteModel(id=note_id, **note_data)
       
-    db.session.add(note)
+    db.session.add(avaliation)
     db.session.commit()
     
-    return note
+    return avaliation
 
 @blp.route("/note")
 class NoteList(MethodView):
-  @blp.response(200, PlainStudant(many=True))
+  @blp.response(200, PlainNoteSchema(many=True))
   def get(self):
-    return NoteModel.query.all()
+    notes = NoteModel.query.all()
+    for note in notes:
+      print(note.studant)
     
-  @blp.arguments(PlainStudant)
-  @blp.response(200, PlainStudant)
+    return notes
+    
+  @blp.arguments(NoteSchema)
+  @blp.response(201, NoteSchema)
   def post(self, note_data):
     note = NoteModel(**note_data)
-      
+
     try:
       db.session.add(note)
       db.session.commit()
-    except SQLAlchemyError:
-      abort(500, message="An error occurred while inserting the note")
+    except SQLAlchemyError as e:
+      abort(500, message=f"An error occurred while inserting the note {str(e)}")
+      db.session.rollback()
+    
+    return note
