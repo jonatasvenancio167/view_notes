af896dd5fb216a8d21f655308245552f8eb000ea
Samila Barros <jvenancio167@hotmail.com>
2023-05-31 20:28:09 -0300

    change schema

--- schemas.py
+++ schemas.py
@@ -1,13 +1,25 @@
 from marshmallow import Schema, fields
 
 class PlainStudant(Schema):
-  id = fields.Str(dump_only=True)
+  id = fields.Int(required=True)
+  cpf = fields.Str(required=True)
   name = fields.Str(required=True)
   birthdate = fields.Str(required=True)
-  status = fields.Bool(required=True)
-  enrollment = fields.Str(required=True)
+  sex = fields.Str(required=True)
+  age = fields.Str(required=True)
   
+class PlainNoteSchema(Schema):
+  id = fields.Int(dump_only=True)
+  note_1 = fields.Float(required=True)
+  note_2 = fields.Float(required=False)
+  finally_note = fields.Float(required=False)
+
 class StudantUpdateSchema(Schema):
   name = fields.Str(required=True)
-  status = fields.Bool(required=True)
   
+class StudantSchema(PlainStudant):
+  notes = fields.List(fields.Nested(lambda: PlainNoteSchema()), dump_only=True)
+  
+class NoteSchema(PlainNoteSchema):
+  studant_id = fields.Int(required=True, load_only=True)
+  studant = fields.Nested(lambda: PlainNoteSchema(), dump_only=True)
