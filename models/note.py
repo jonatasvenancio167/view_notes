18cae2d6595ee6ed81bdf3bd222c180e88525c0f
Jônatas Venâncio <jvenancio167@hotmail.com>
2023-06-08 22:22:48 -0300

    creating methods to calc average and final result

--- models/note.py
+++ models/note.py
@@ -1,5 +1,5 @@
+from decimal import Decimal
 from db import db
-from sqlalchemy import text
 
 class NoteModel(db.Model):
   __tablename__ = 'notes'
@@ -9,6 +9,7 @@ class NoteModel(db.Model):
   note_1 = db.Column(db.Float(precision=2), default=0.0)
   note_2 = db.Column(db.Float(precision=2), default=0.0)
   finally_note = db.Column(db.Float(precision=2), default=0.0)
+  status = db.Column(db.String())
   
   studant_id = db.Column(
 		db.Integer, db.ForeignKey("studants.id"), nullable=False
@@ -20,5 +21,23 @@ class NoteModel(db.Model):
       "id": self.id,
       "note_1": self.note_1,
       "note_2": self.note_2,
-      "finally_note": self.finally_note,
+      "finally_note": self.calculate_final_grade(),
+      "status" : self.final_result()
     }
+  
+  def calculate_final_grade(self):
+    average = sum([Decimal(self.note_1), Decimal(self.note_2)]) / 2
+    self.finally_note = round(average, 1)
+    
+    return self.finally_note
+  
+  def final_result(self):
+    result = ""
+    
+    if self.note_1 > 0.0 and self.note_2 > 0.0 and self.finally_note >= 6:
+      result = 'approved'
+    else:
+      result = 'reject'
+      
+    return result
+    
